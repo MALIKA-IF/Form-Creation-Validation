d71d41354f0aebd105958532ef40f912203a2ef3
Malika Edaif <edmalika957@gmail.com>
2025-08-03 19:13:27 +0100

    task1

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", console.log("page loaded"));
+
+const form =document.getElementById("#registration-form");
+
+const feedbackDiv=document.querySelector("#form-feedback");
+
