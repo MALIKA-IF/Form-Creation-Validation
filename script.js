f46ec8bab23b295e55e31be2fc12cfe3d614ce63
Malika Edaif <edmalika957@gmail.com>
2025-08-24 16:32:25 +0100

    handle the input submition

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const { use } = require("react");
+
 document.addEventListener('DOMContentLoaded',function(){
 
     const form =document.getElementById('registration-form');
@@ -7,6 +9,42 @@ document.addEventListener('DOMContentLoaded',function(){
     form.addEventListener('submit', function(event){
 
         event.preventDefault();
+       const username = document.getElementById('username').value.trim();
+       const email =document.getElementById('email').value.trim();
+       const password = document.getElementById('password').value.trim();
+
+       let isvalid=true;
+
+       let messages =[];
+
+       //validation username
+       
+       if (username.lenght() != 3){
+        isvalid=false;
+
+        messages.push('Error!');
+       }
+       //validation email
+
+       if (!email.includes('@') || !email.includes('.')){
+           isvalid=false;
+           messages.push("the email doesn't contain the @ or .");
+       }
+
+       //validation password
+
+       if (password.lenght() != 8)
+
+        isvalid=false;
+        messages.push('the password should have at least 8 caracters');
+
         
+
     })
+
+    
+
+
+
+    
 })
